cfd5efee9323a6437bf48937a2926e9a7fe87cf3
Aigul <sharipova.aig@gmail.com>
2022-06-20 19:09:30 -0400

    let

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ const back = document.querySelector('#back');
 const next = document.querySelector('#next');
 
 const picture = ['grand.jpg', 'olympic.jpg', 'Yellowstone.jpg', 'yosemite.jpg', 'zion.jpg'];
+let i = 0;
