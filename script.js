74e9e3135a0e22cf4382e99068462d70ad207f9b
Aigul <sharipova.aig@gmail.com>
2022-06-20 19:27:11 -0400

    picture next

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const back = document.querySelector('#back');
 const next = document.querySelector('#next');
 
-const picture = ['grand.jpg', 'olympic.jpg', 'Yellowstone.jpg', 'yosemite.jpg', 'zion.jpg'];
+const photos = ['./style/grand.jpg', './style/olympic.jpg', './style/Yellowstone.jpg', './style/yosemite.jpg', './style/zion.jpg'];
 let i = 0;
-next.addEventListener('click', ()=> {
+next.addEventListener('click', () => {
   i++;
-  document.querySelector('#picture').src = picture[0];
+  document.querySelector('#picture').src = photos[i];
 })
