d5e7e5b07334db04e092e217847b64583ff96556
Aigul <sharipova.aig@gmail.com>
2022-06-20 20:18:30 -0400

    back  array

--- script.js
+++ script.js
@@ -6,11 +6,14 @@ let i = 0;
 next.addEventListener('click', () => {
   i++;
   if (i > photos.length - 1)
-  i = 0
+  i = 0;
   document.querySelector('#picture').src = photos[i];
 })
 
 back.addEventListener('click', () => {
   i--;
+  if (i < 0) {
+    i = photos.length - 1;
+  }
   document.querySelector('#picture').src = photos[i];
 })
